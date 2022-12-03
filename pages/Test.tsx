96c26d809634457c3425d86dbbf0ffe703245bca
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-12-03 14:29:45 +0530

    updates

--- pages/Test.tsx
+++ pages/Test.tsx
@@ -1,58 +1,65 @@
-import React from 'react'
+// @ts-ignore
+import React,{useContext,useState,useEffect} from 'react'
+import { AuthContext,AuthContextProps } from '../context/AuthContext'
+import * as PushAPI from "@pushprotocol/restapi";
+import * as ethers from "ethers";
 
-import GuildListItem from '../components/GuildListItem'
 function Test() {
-  const [guilds, setGuilds] = React.useState([{
-    id:1,
-    name: 'Vishnu Shon',
-    followers: 2500000,
-    capital: 2000000,
-    rank: 18,
-    image: '/User.png',
-    follow: '/Follow.png'
-  }])
-  const [name, setName] = React.useState('')
-  const [filteredGuilds, setFilteredGuilds] = React.useState(guilds)
+  const [address,setAddress] = useState<string>('')
+  const [notification,setNotification] = useState<any>(null)
+  const{account} = useContext<AuthContextProps>(AuthContext)
 
+  const PK = ''
+  const Pkey = `0x${PK}`;
+  const signer = new ethers.Wallet(Pkey);
 
-  const searchGuilds = (e:any) => {
-    const searchValue = e.target.value
-    setName(searchValue)
-    const newGuilds = guilds.filter((guild) => {
-      return guild.name.toLowerCase().includes(searchValue.toLowerCase())
-    })
-    // if (newGuilds) {
-    // if (newGuilds.length > 0) {
-      setFilteredGuilds(newGuilds)
-      console.log(newGuilds)
-    // }
-    // else {
-    //   setFilteredGuilds(guilds)
-    // }
+  const reciveResponse = async () => {
+    const notifications = await PushAPI.user.getFeeds({
+      user: 'eip155:5:0x6C59eC34920Eb91c408A31c871532859787eBFE8', // user address in CAIP
+      env: 'staging'
+    });
+    console.log(notifications)
+    setNotification(notifications)
+  }
+
+  const sendNotification = async() => {
+    try {
+      const apiResponse = await PushAPI.payloads.sendNotification({
+        signer,
+        type: 3, // target
+        identityType: 2, // direct payload
+        notification: {
+          title: `sending notif:`,
+          body: `hi bro`
+        },
+        payload: {
+          title: `hi bro`,
+          body: `Hello bro!!!`,
+          cta: '',
+          img: ''
+        },
+        recipients: 'eip155:5:0x6C59eC34920Eb91c408A31c871532859787eBFE8', // recipient address
+        channel: 'eip155:5:0x6C59eC34920Eb91c408A31c871532859787eBFE8', // your channel address
+        env: 'staging'
+      });
+      
+      // apiResponse?.status === 204, if sent successfully!
+      console.log('API repsonse: ', apiResponse);
+    } catch (err) {
+      console.error('Error: ', err);
+    }
   }
 
 
   return (
-    <div className="relative h-screen overflow-hidden  bg-no-repeat	bg-fixed px-52">
-      <div className="flex pt-10 border mt-[100px] mx-[400px] justify-center">
-        <input 
-        className="flex-1 w-max h-20 bg-textAccent rounded-xl p-10"
-        type="text"
-        placeholder="Search"
-        value={name}
-        onChange={searchGuilds}
-        >
-        </input>
-      </div>
-      <div className='m-10'>
-        {
-          filteredGuilds.map((guild) => {
-            return (
-              <GuildListItem key={guild.id} guild={guild}/>
-              )
-            })
-          }
-        </div>
+    <div className="relative h-screen overflow-hidden flex flex-col items-center justify-center  bg-no-repeat	bg-fixed px-52">
+      <p>from: {account}</p>
+      <input value={address} onChange={(e)=>setAddress(e.target.value)} placeholder="to: " className="my-10"/>
+      <button className="bg-secondary p-3 rounded-lg " onClick={sendNotification}>send notif</button>
+
+      <p className="my-10">Your Notification</p>
+      <button className="bg-secondary p-3 rounded-lg " onClick={reciveResponse}>rcv notif</button>
+      <div>{notification}</div>
     </div>
   )
 }
